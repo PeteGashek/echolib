5818874e5a0ca13fd7e7808278b0383ea41cfa80
dwdamon <ddamon@LW100227.covisint.corp>
2015-02-25 14:45:23 -0500

    initial unit test for EchoTodoItemBase class

---
+++ src/test/java/com/javasteam/amazon/echo/EchoTodoItemBaseTest.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.javasteam.amazon.echo;
+
+import junit.framework.TestCase;
+import org.apache.http.impl.client.BasicCookieStore;
+
+/**
+ * @author ddamon
+ *
+ */
+public class EchoTodoItemBaseTest extends TestCase {
+
+  /**
+   * 
+   */
+  public EchoTodoItemBaseTest() {
+  }
+
+  /**
+   * @param name
+   */
+  public EchoTodoItemBaseTest( String name ) {
+    super( name );
+  }
+
+  /**
+   * 
+   */
+  public void testEchoTodoItemBase() throws Exception {
+    // test constructor with text passed in as argument
+    EchoTodoItemBase todoItem = new EchoTodoItemBase( "Test todo item" );
+    
+    assertEquals( "Test todo item", todoItem.getText() );
+    assertEquals( null,             todoItem.getItemId() );
+    assertEquals( null,             todoItem.getType() );
+    assertEquals( null,             todoItem.getUtteranceId() );
+    assertEquals( null,             todoItem.getCreatedDate() );
+    assertEquals( null,             todoItem.getLastLocalUpdateDate() );
+    assertEquals( null,             todoItem.getLastUpdatedDate() );
+    assertEquals( null,             todoItem.getNbestItems() );
+    assertEquals( null,             todoItem.getVersion() );
+    
+    // constructor with itemId and text parameters
+    todoItem = new EchoTodoItemBase( "ITEMID", "Test todo item" );
+    
+    assertEquals( "Test todo item", todoItem.getText() );
+    assertEquals( "ITEMID",         todoItem.getItemId() );
+    assertEquals( null,             todoItem.getType() );
+    assertEquals( null,             todoItem.getUtteranceId() );
+    assertEquals( null,             todoItem.getCreatedDate() );
+    assertEquals( null,             todoItem.getLastLocalUpdateDate() );
+    assertEquals( null,             todoItem.getLastUpdatedDate() );
+    assertEquals( null,             todoItem.getNbestItems() );
+    assertEquals( null,             todoItem.getVersion() );
+  }
+}
